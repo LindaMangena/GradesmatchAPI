86d3c4883bdd2a7d555ad62c6a42e1e5262f5cde
LindaMangena <linda.mangena@barron.com>
2024-08-19 00:11:27 +0200

    UI changes

--- obj/Debug/net8.0/GradesmatchAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GradesmatchAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradesmatchAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46bedf545990a4d854e4208d4ff8b0667510f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradesmatchAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradesmatchAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
